285ac671801cbb44f6f630d25834e49dc4241040
BrandonMcPherson <brandon.mcpherson96@yahoo.com>
2022-10-30 18:15:06 -0500

    Added Files

---
+++ scripts.js
@@ -0,0 +1,15 @@
+/* Add your JavaScript to this file */
+
+window.onload = function(){
+  submit = document.getElementsByTagName("button")[0]
+  message = document.getElementsByClassName("message")[0]
+  submit.setAttribute('type', 'button')
+
+
+  submit.addEventListener("click", function(){
+    email = document.getElementsByTagName("input")[0].value
+    if ((email.includes("@")) && (email.includes("."))){message.innerHTML = "Thank you! Your email " +
+      email.toString() + " has been added to our mailing list!"}
+    else{message.innerHTML = "Error: Please enter valid email address."}
+  })
+}
